af45982b4ea1e9b9d5aa50f953524467aa78cba2
Mathias Fuchs <mathias_fuchs_87@yahoo.de>
2021-03-12 13:34:29 +0100

    added css, design and layout almost done

--- scripts/script.js
+++ scripts/script.js
@@ -12,28 +12,43 @@ function movieGeneration (movies){
         
         cards += `
         
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${movies[i].image} " alt="...">
+        
+        
+        <div class="card m-4 bg-dark text-white" style="max-width: 540px;">
+            <div class="row">
+                
+            <div class="col-md-4">
+                    <img src="${movies[i].image}">
                 </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h3 class="card-title">${movies[i].movieName}  </h3>
-                        <p class="card-text">${movies[i].description} </p>
+                    
+                    <div class="col-md-8">
+                        <div class="card-body height">
+                            <h3 class="card-title">${movies[i].movieName}</h3>
+                            <p class="card-text">${movies[i].description} </p>
+                            
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item bg-secondary">Length: ${movies[i].length}</li>
+                                <li class="list-group-item bg-secondary">Genre(s): ${movies[i].genre}</li>
+                                <li class="list-group-item bg-secondary">Release: ${movies[i].release}</li>
+                            </ul>
+                            
+                        </div>
                     </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Length: ${movies[i].length}</li>
-                            <li class="list-group-item">Genre(s): ${movies[i].genre} </li>
-                            <li class="list-group-item">Release: ${movies[i].release} </li>
-                        </ul>
-                    <div class="card-body">
-                    <h3>Likes<span class="badge bg-secondary"> ${movies[i].likes}</span></h3>
+
+                    <div class="card-btm d-flex justify-content-evenly">
+                        
+                        <button class="btn-own text-success" style="background-color: transparent;">
+                            <i class="fas fa-thumbs-up"></i> Like
+                        </button>  
+                       
+                        <span class="badge text-success bg-light">${movies[i].likes}</span>
+                        
                     </div>
-                </div>
+
             </div>
         </div>
-       
+            
+        
 
         `
     
